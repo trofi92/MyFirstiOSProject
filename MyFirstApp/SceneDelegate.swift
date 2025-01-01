20928c62d8d4ad6652e709ce9e650985e93af15a
SY <lunaslash9249@gmail.com>
2025-01-01 17:07:25 +0900

    first iOS Project for Practice : day001 : 프로젝트 생성 및 Xcode 둘러보기, Hot Reload사용 위한 InjectionIII 세팅

--- MyFirstApp/SceneDelegate.swift
+++ MyFirstApp/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Inject
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,12 +14,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
-
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            
+            window = UIWindow(windowScene: windowScene)
+            
+            #if DEBUG
+                // 개발 환경에서만 Inject 사용
+                let rootViewController = Inject.ViewControllerHost(ViewController())
+            #else
+                // 배포 환경에서는 일반적인 방식으로 초기화
+                let rootViewController = ViewController()
+            #endif
+            
+            let navigationController = UINavigationController(rootViewController: rootViewController)
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello Swift!"
+        label.textColor = .red
+        self.view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
